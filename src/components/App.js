bf55f88cc0c6b47163e7328520dee1945b9f5902
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-10-28 21:27:25 +0530

    Refactor App component and ButtonCounter function

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-
-import React from "react";
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
-const App = () => {
-  function ButtonCounter() {
+const ButtonCounter = () => {
   const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    setCount(count + 1); 
+    setCount(count + 1);
   };
+
   return (
     <div>
       <p>Button clicked {count} times</p>
       <button onClick={handleClick}>Click Me</button>
     </div>
-  )
-}
-            
-export default App
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <ButtonCounter />
+    </div>
+  );
+};
+
+export default App;
